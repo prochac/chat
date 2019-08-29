139ab9b33c5c5afb71ce1311c32ad036b34c6ec2
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2019-08-29 15:54:32 +0200

    receive first message

--- main.go
+++ main.go
@@ -7,8 +7,21 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w,"Hello, world")
+	http.HandleFunc("/post-message", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		r.ParseForm()
+		fmt.Print(r.Form)
+
+		http.Redirect(w, r, "/", http.StatusFound)
 	})
+
+	http.Handle("/",
+		http.FileServer(http.Dir("web")),
+	)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

dd6676c622570c1fa8eda4c8a6dd3c8135e97b72
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2019-08-29 16:18:04 +0200

    show messages

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
+	var messages []string
+
 	http.HandleFunc("/post-message", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
@@ -14,13 +16,37 @@ func main() {
 		}
 
 		r.ParseForm()
-		fmt.Print(r.Form)
+		if len(r.Form["message"]) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("no message"))
+			return
+		}
+
+		messages = append(messages, r.Form["message"][0])
 
 		http.Redirect(w, r, "/", http.StatusFound)
 	})
 
-	http.Handle("/",
-		http.FileServer(http.Dir("web")),
+	http.Handle("/", func() http.HandlerFunc {
+		t, err := template.ParseFiles("web/index.html")
+		if err != nil {
+			panic(err)
+		}
+		return func(w http.ResponseWriter, r *http.Request) {
+			data := struct {
+				Messages []string
+			}{
+				Messages: messages,
+			}
+
+			t.Execute(w, data)
+		}
+	}())
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("web/static")),
+		),
 	)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
